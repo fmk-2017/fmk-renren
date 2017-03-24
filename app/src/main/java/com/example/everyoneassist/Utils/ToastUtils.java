26d0e05281451a4a13ca5966adf1e53314e490bd
pig112200 <1196836756@qq.com>
2017-03-24 21:11:20 +0800

    随意购
    随意购界面完成
    页面逻辑完成
    接口没有

---
+++ app/src/main/java/com/example/everyoneassist/Utils/ToastUtils.java
@@ -0,0 +1,16 @@
+package com.example.everyoneassist.Utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2017/3/23.
+ */
+
+public class ToastUtils {
+
+    public static void tipShort(Context context,String tipStr){
+        Toast.makeText(context,"请填写收货人姓名",Toast.LENGTH_LONG).show();
+    }
+
+}

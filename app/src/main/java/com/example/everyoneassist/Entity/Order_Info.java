4825622268e576ac850bd7b9a7652a0ae881ec2e
fengmekan <fengmeikan@hotmail.com>
2017-04-10 03:29:30 +0800

    4月10早 0339

---
+++ app/src/main/java/com/example/everyoneassist/Entity/Order_Info.java
@@ -0,0 +1,280 @@
+package com.example.everyoneassist.Entity;
+
+/**
+ * Created by fengm on 2017-4-10.
+ */
+
+public class Order_Info {
+
+    private String id;
+    private String user_id;
+    private String category_id;
+    private String server_sex;
+    private String server_day;
+    private String server_type;
+    private String server_lon;
+    private String server_lat;
+    private String server_price;
+    private String server_tag;
+    private String address;
+    private String info;
+    private String is_accomplish;
+    private String status;
+    private String addtime;
+    private String cat_name;
+    private String address_id;
+    private String address_name;
+    private String consignee;
+    private String email;
+    private String country;
+    private String province;
+    private String city;
+    private String district;
+    private String zipcode;
+    private String tel;
+    private String mobile;
+    private String sign_building;
+    private String best_time;
+    private String server_type_name;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(String category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getServer_sex() {
+        return server_sex;
+    }
+
+    public void setServer_sex(String server_sex) {
+        this.server_sex = server_sex;
+    }
+
+    public String getServer_day() {
+        return server_day;
+    }
+
+    public void setServer_day(String server_day) {
+        this.server_day = server_day;
+    }
+
+    public String getServer_type() {
+        return server_type;
+    }
+
+    public void setServer_type(String server_type) {
+        this.server_type = server_type;
+    }
+
+    public String getServer_lon() {
+        return server_lon;
+    }
+
+    public void setServer_lon(String server_lon) {
+        this.server_lon = server_lon;
+    }
+
+    public String getServer_lat() {
+        return server_lat;
+    }
+
+    public void setServer_lat(String server_lat) {
+        this.server_lat = server_lat;
+    }
+
+    public String getServer_price() {
+        return server_price;
+    }
+
+    public void setServer_price(String server_price) {
+        this.server_price = server_price;
+    }
+
+    public String getServer_tag() {
+        return server_tag;
+    }
+
+    public void setServer_tag(String server_tag) {
+        this.server_tag = server_tag;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getIs_accomplish() {
+        return is_accomplish;
+    }
+
+    public void setIs_accomplish(String is_accomplish) {
+        this.is_accomplish = is_accomplish;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddtime() {
+        return addtime;
+    }
+
+    public void setAddtime(String addtime) {
+        this.addtime = addtime;
+    }
+
+    public String getCat_name() {
+        return cat_name;
+    }
+
+    public void setCat_name(String cat_name) {
+        this.cat_name = cat_name;
+    }
+
+    public String getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(String address_id) {
+        this.address_id = address_id;
+    }
+
+    public String getAddress_name() {
+        return address_name;
+    }
+
+    public void setAddress_name(String address_name) {
+        this.address_name = address_name;
+    }
+
+    public String getConsignee() {
+        return consignee;
+    }
+
+    public void setConsignee(String consignee) {
+        this.consignee = consignee;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getSign_building() {
+        return sign_building;
+    }
+
+    public void setSign_building(String sign_building) {
+        this.sign_building = sign_building;
+    }
+
+    public String getBest_time() {
+        return best_time;
+    }
+
+    public void setBest_time(String best_time) {
+        this.best_time = best_time;
+    }
+
+    public String getServer_type_name() {
+        return server_type_name;
+    }
+
+    public void setServer_type_name(String server_type_name) {
+        this.server_type_name = server_type_name;
+    }
+}

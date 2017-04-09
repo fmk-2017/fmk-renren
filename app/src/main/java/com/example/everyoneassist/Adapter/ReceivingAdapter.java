f4cf04569f822f59fe98cfd6bef0d803f78ea46a
pig112200 <1196836756@qq.com>
2017-04-09 18:51:19 +0800

    立即应邀

--- app/src/main/java/com/example/everyoneassist/Adapter/ReceivingAdapter.java
+++ app/src/main/java/com/example/everyoneassist/Adapter/ReceivingAdapter.java
@@ -2,6 +2,7 @@ package com.example.everyoneassist.Adapter;
 
 import android.content.Context;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.everyoneassist.Entity.Demand;
+import com.example.everyoneassist.Interface.OnInvitedListener;
 import com.example.everyoneassist.R;
 import com.example.everyoneassist.Utils.TimeUtils;
 import com.example.everyoneassist.View.CircleImageView;
@@ -24,14 +26,16 @@ public class ReceivingAdapter extends BaseAdapter implements View.OnClickListene
 
     private Context context;
     private List<Demand> demandList;
+    private OnInvitedListener invitedListener;
 
     public ReceivingAdapter(Context context) {
         this.context = context;
     }
 
-    public ReceivingAdapter(Context context, List<Demand> demandList) {
+    public ReceivingAdapter(Context context, List<Demand> demandList,OnInvitedListener invitedListener) {
         this.context = context;
         this.demandList = demandList;
+        this.invitedListener =  invitedListener;
     }
 
     @Override
@@ -90,18 +94,16 @@ public class ReceivingAdapter extends BaseAdapter implements View.OnClickListene
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.receiving:
+            case R.id.receiving://立即应邀
                 int position = (int) v.getTag();
-
+                if (invitedListener != null)
+                    invitedListener.onInvited(position);
                 break;
         }
-
-
     }
 
     class ViewHolder {
         TextView listview_title, user_name, bestows_num, work, work_content, work_time, work_address, work_price, receiving;
         CircleImageView avatar;
     }
-
 }
--- app/src/main/java/com/example/everyoneassist/Fragment/ReceivingFragment.java
+++ app/src/main/java/com/example/everyoneassist/Fragment/ReceivingFragment.java
@@ -32,6 +32,7 @@ import com.amap.api.maps2d.model.MyLocationStyle;
 import com.example.everyoneassist.Adapter.ReceivingAdapter;
 import com.example.everyoneassist.Entity.Demand;
 import com.example.everyoneassist.Entity.GoodsNameAndId;
+import com.example.everyoneassist.Interface.OnInvitedListener;
 import com.example.everyoneassist.R;
 import com.example.everyoneassist.Utils.DebugLog;
 import com.example.everyoneassist.Utils.HttpPostRequestUtils;
@@ -48,8 +49,9 @@ import java.util.HashMap;
 import java.util.List;
 
 
-public class ReceivingFragment extends Fragment implements LocationSource, AMapLocationListener, HttpPostRequestUtils.HttpPostRequestCallback, View.OnClickListener {
+public class ReceivingFragment extends Fragment implements LocationSource, AMapLocationListener, HttpPostRequestUtils.HttpPostRequestCallback, View.OnClickListener,OnInvitedListener {
     private final String METHOD_DEMAND = "demand_list";
+    private final String APPOINTMENT_SERVIER = "appointment_server";
     private MapView d2map;
     private MyListView receiving_listview;
     private ReceivingAdapter receivingAdapter;
@@ -211,8 +213,10 @@ public class ReceivingFragment extends Fragment implements LocationSource, AMapL
     public void Success(String method, JSONObject json) throws JSONException {
         if (METHOD_DEMAND.equals(method)) {
             demandList = JSON.parseArray(json.getString("data"), Demand.class);
-            receivingAdapter = new ReceivingAdapter(getActivity(), demandList);
+            receivingAdapter = new ReceivingAdapter(getActivity(), demandList,this);
             receiving_listview.setAdapter(receivingAdapter);
+        }else if(APPOINTMENT_SERVIER.equals(mode)){
+
         }
     }
 
@@ -254,4 +258,16 @@ public class ReceivingFragment extends Fragment implements LocationSource, AMapL
     }
 
 
+    //立即应邀
+    @Override
+    public void onInvited(int position) {
+        if(demandList != null){
+            Demand demand = demandList.get(position);
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("act", APPOINTMENT_SERVIER);
+            map.put("user_id", "1");
+            map.put("server_id", "1");
+            HttpPostRequestUtils.getInstance(this).Post(map);
+        }
+    }
 }
